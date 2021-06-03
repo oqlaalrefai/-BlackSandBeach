90d49e5fb253ea2d633277094bf0f1769356d025
oqlaalrefai <72412478+oqlaalrefai@users.noreply.github.com>
2021-06-03 18:29:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ x = prompt("enter the number of beach that you want to see")
 
 
 if (x == 1) {
-  document.write('<img src="https://www.usnews.com/dims4/USNEWS/543934b/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F0b%2F97%2Feaa9549e423f925510656e7fb3c2%2F6-spain-beach-getty.jpg" height="100px" width="100px" /> \n')
+  document.write('<img src="https://www.usnews.com/dims4/USNEWS/543934b/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F0b%2F97%2Feaa9549e423f925510656e7fb3c2%2F6-spain-beach-getty.jpg" height="100px" width="100px" /> <br/>')
 
 }
 else if (x == 2) {
-  document.write('<img src="https://www.usnews.com/dims4/USNEWS/bb261e2/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F6a%2Fb3%2F3fe89a2a47f7ac8ee912fe459055%2F7-yellowstone-getty.jpg" height="100px" width="100px" /> \n')
+  document.write('<img src="https://www.usnews.com/dims4/USNEWS/bb261e2/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F6a%2Fb3%2F3fe89a2a47f7ac8ee912fe459055%2F7-yellowstone-getty.jpg" height="100px" width="100px" /> <br/>')
 
 }
 
