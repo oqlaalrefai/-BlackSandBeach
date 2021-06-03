126293a7d343878ec8251a0cc6abd62f0f77dac5
oqlaalrefai <72412478+oqlaalrefai@users.noreply.github.com>
2021-06-03 18:34:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ x = prompt("enter the number of beach that you want to see")
 
 
 
-document.write('  this   image  that   you  want   to show you from old assignment <br/><br/>')
 if (x == 1) {
+document.write('  this   image  that   you  want   to show you from old assignment <br/><br/>')
   document.write('<img src="https://www.usnews.com/dims4/USNEWS/543934b/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F0b%2F97%2Feaa9549e423f925510656e7fb3c2%2F6-spain-beach-getty.jpg" height="100px" width="100px" /> <br/>')
 
 }
 else if (x == 2) {
+document.write('  this   image  that   you  want   to show you from old assignment <br/><br/>')
   document.write('<img src="https://www.usnews.com/dims4/USNEWS/bb261e2/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F6a%2Fb3%2F3fe89a2a47f7ac8ee912fe459055%2F7-yellowstone-getty.jpg" height="100px" width="100px" /> <br/>')
 
 }
