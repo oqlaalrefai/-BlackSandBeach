0341214c5d5dfdf9842fc9e67b3f5db52127ad93
oqlaalrefai <>
2021-06-06 11:29:48 +0000

    edit

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-alert("1.El Médano Beach     2.Black Sand Basin")
+function welcome(){
+alert("welcom in my webpage \n 1.El Médano Beach     2.Black Sand Basin")
 x = prompt("enter the number of beach that you want to see")
-
-
-
-
 
 if (x == 1) {
 document.write('  this   image  that   you  want   to show you from old assignment <br/>')
@@ -19,10 +16,17 @@ document.write('  this   image  that   you  want   to show you from old assignme
 else {
   alert('you entered incorrect value')
 }
-var y =prompt("enter number of images that you want to show")
 
+}
+
+function showImage(y){
 while(y<0 || y>11)
   {y= prompt('please enter value betwee 1 and 10')}
 
 for (i=0;i<y;i++)
     {  document.write('<br/> <img src="https://www.usnews.com/dims4/USNEWS/df913ab/2147483647/resize/1200x%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2F74%2F3f%2F6b732db34d73bdc0263f661a7963%2F10-guatemala-getty.jpg" height="100px" width="100px" />')}
+}
+welcome()
+
+
+showImage(y =prompt("enter number of images that you want to show"))
